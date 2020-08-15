d23c838ce9111387351451d423a2276edc795ea0
Jared Hughes <jahughes241@gmail.com>
2020-08-14 23:56:44 -0700

    Apply keystrokes to host computer

--- webserver.js
+++ webserver.js
@@ -1,6 +1,9 @@
 let http = require('http').createServer(handler);
 let fs = require('fs');
 let io = require('socket.io')(http);
+let robot = require("robotjs");
+
+console.log("[INFO] Libraries loaded.")
 
 http.listen(8000);
 
@@ -13,19 +16,53 @@ function handler(req, res) {
   });
 }
 
+robot.setKeyboardDelay(0);
+
+// input: javascript key event key names (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values)
+// output: robotjs key names (https://robotjs.io/docs/syntax#keyboard)
+// keyMapping currently set up for Duck Game controls (https://duckgame.fandom.com/wiki/Controls)
+// TODO: proper configuration menu
+keyMapping = {
+  "w": "up",
+  "a": "left",
+  "s": "down",
+  "d": "right",
+  "b": "k",
+  "q": "i",
+  "c": "l",
+  "v": ";",
+  "Escape": "+",
+  " ": "right_shift",
+  "e": "o"
+}
+
+console.log("[INFO] Listening ready.")
+
+// concurrent connections have control over separate keypressed state
+// but both control the same robot keys
 io.sockets.on('connection', socket => {
   let pressed = [];
 
   function keyDown(key) {
     console.log("v", key, pressed);
+    // pun!
+    if (key in keyMapping) {
+      console.log(keyMapping[key])
+      robot.keyToggle(keyMapping[key], "down");
+    }
   }
 
   function keyUp(key) {
     console.log("^", key, pressed);
+    if (key in keyMapping) {
+      console.log(keyMapping[key])
+      robot.keyToggle(keyMapping[key], "up");
+    }
   }
 
   function keyRepeat(key) {
     console.log(".", key, pressed);
+    // ignore for now
   }
 
   socket.on('keydown', key => {
